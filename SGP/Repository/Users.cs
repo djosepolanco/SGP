628529f37e9a5bb021d577e28b9a2f6f73e88bf8
dpolanco14 <delciopolanco@gmail.com>
2017-09-27 23:27:10 -0400

    Agregando fucionalidades a los clientes, modificacion del main y ffunciinalidades

---
+++ SGP/Repository/Users.cs
@@ -0,0 +1,32 @@
+﻿using SGP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGP.Repository
+{
+    public class UsersRepository
+    {
+        /// <summary>
+        ///         /// Autentica el usuario contra el sistema utilizando credenciales utilizando usuario y contraseña
+        /// </summary>
+        /// <param name="userName"> Nombre de usuario</param>
+        /// <param name="passWord"> Contraseña del usuario</param>
+        /// <returns> Retorna Boolean True o False si fue logueado o no </returns>
+        public Boolean Login(string userName, string passWord)
+        {
+            bool isAuthenticated = false;
+
+            using (var DB = new DBContext())
+            {
+                isAuthenticated = DB.Users.Where(
+                u => u.UserName.Equals(userName) &&
+                u.PassWord.Equals(passWord)).Count() > 0;
+            }
+
+            return isAuthenticated;
+        }
+    }
+}
